1592603f4b408c25c0d381f7619d355320f1bfb8
oatp1029 <wiljo463@gmail.com>
2024-02-17 20:59:35 -0800

    24/02/17 fixed main page

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,18 @@
+const headerBtn = document.querySelector(".mobile-btn");
+const headerMenu = document.querySelector(".fixed-header > .menu");
+
+headerBtn.addEventListener("click", () => {
+  headerMenu.classList.toggle("show");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.screen.width <= 767) {
+    document.querySelector(".fixed-header").classList.add("mobile");
+  } else {
+    document.querySelector(".fixed-header").classList.remove("mobile");
+  }
+});
+
 window.onload = function () {
   document.querySelector(".background-img").classList.add("show");
 };
