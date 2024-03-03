6e24d355c4dd505a8bef07c4e3893ae52bf1c38b
oatp1029 <wiljo463@gmail.com>
2024-03-02 21:01:09 -0800

    240302 2100 About Page Update

--- Assets/script.js
+++ Assets/script.js
@@ -5,14 +5,18 @@ headerBtn.addEventListener("click", () => {
   headerMenu.classList.toggle("show");
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (window.screen.width <= 767) {
+const fixedHeaderSet = () => {
+  if (document.documentElement.clientWidth <= 767) {
     document.querySelector(".fixed-header").classList.add("mobile");
   } else {
     document.querySelector(".fixed-header").classList.remove("mobile");
   }
-});
+};
+
+document.addEventListener("DOMContentLoaded", fixedHeaderSet);
 
 window.onload = function () {
   document.querySelector(".background-img").classList.add("show");
 };
+
+window.addEventListener("resize", fixedHeaderSet);
